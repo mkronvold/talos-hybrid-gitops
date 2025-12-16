30b787c562ce03249d1fa519e4c82d0622bbb9f1
Michael W Kronvold <michael.kronvold@e2open.com>
2025-12-16 15:53:57 -0600

    script to create cloudinit enabled ubuntu template for jumphost

---
+++ scripts/ubuntu-noble-cloudinit.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+set -xe
+
+VMID="${VMID:-7999}"
+STORAGE="${STORAGE:-zfs}"
+GITHUBUSER=mkronvold
+
+IMG="noble-server-cloudimg-amd64.img"
+BASE_URL="https://cloud-images.ubuntu.com/noble/current"
+EXPECTED_SHA=$(wget -qO- "$BASE_URL/SHA256SUMS" | awk '/'$IMG'/{print $1}')
+
+download() {
+    wget -q "$BASE_URL/$IMG"
+}
+
+verify() {
+    sha256sum "$IMG" | awk '{print $1}'
+}
+
+[ ! -f "$IMG" ] && download
+
+ACTUAL_SHA=$(verify)
+
+if [ "$EXPECTED_SHA" != "$ACTUAL_SHA" ]; then
+    rm -f "$IMG"
+    download
+    ACTUAL_SHA=$(verify)
+    [ "$EXPECTED_SHA" != "$ACTUAL_SHA" ] && exit 1
+fi
+
+rm -f noble-server-cloudimg-amd64-resized.img
+cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-resized.img
+qemu-img resize noble-server-cloudimg-amd64-resized.img 32G
+
+qm destroy $VMID || true
+qm create $VMID --name "ubuntu-noble-template" --ostype l26 \
+    --memory 1024 --balloon 0 \
+    --agent 1 \
+    --bios ovmf --machine q35 --efidisk0 $STORAGE:0,pre-enrolled-keys=0 \
+    --cpu host --socket 1 --cores 1 \
+    --vga serial0 --serial0 socket  \
+    --net0 virtio,bridge=vmbr0
+qm importdisk $VMID noble-server-cloudimg-amd64-resized.img $STORAGE
+qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
+qm set $VMID --boot order=virtio0
+qm set $VMID --scsi1 $STORAGE:cloudinit
+
+cat << EOF | tee /var/lib/vz/snippets/ubuntu.yaml
+#cloud-config
+runcmd:
+    - apt-get update
+    - apt-get install -y qemu-guest-agent
+    - systemctl enable ssh
+    - reboot
+# Taken from https://forum.proxmox.com/threads/combining-custom-cloud-init-with-auto-generated.59008/page-3#post-428772
+EOF
+
+curl https://github.com/${GITHUBUSER}.keys > /tmp/authorized_keys
+
+qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
+qm set $VMID --tags ubuntu-template,noble,cloudinit
+qm set $VMID --ciuser $USER
+qm set $VMID --sshkeys /tmp/authorized_keys
+qm set $VMID --ipconfig0 ip=dhcp
+qm template $VMID
+
+rm -f /tmp/authorized_keys
